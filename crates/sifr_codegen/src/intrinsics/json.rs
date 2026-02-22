a113b771d9d4ad071c127bd47fcf51d4ec7a74f5
Yaser Al-Najjar <10493809+yaseralnajjar@users.noreply.github.com>
2026-02-22 14:52:08 +0100

    codegen: convert json intrinsic arg wrapper to structured IR (#439)

--- crates/sifr_codegen/src/intrinsics/json.rs
+++ crates/sifr_codegen/src/intrinsics/json.rs
@@ -1,81 +1,89 @@
 //! JSON intrinsic lowerers for registry migration.
 
-use crate::{RustExpr, RustParam, RustType};
+use crate::{RustExpr, RustParam, RustStmt, RustType};
 
 pub(super) fn lower_json_loads(args: &[String]) -> Option<RustExpr> {
     if args.len() != 1 {
         return None;
     }
-    Some(RustExpr::MethodCall {
-        receiver: Box::new(RustExpr::MethodCall {
-            receiver: Box::new(RustExpr::FnCall {
-                func: Box::new(RustExpr::Path(vec![
-                    "serde_json".to_string(),
-                    "from_str::<serde_json::Value>".to_string(),
-                ])),
-                args: vec![RustExpr::MethodCall {
-                    receiver: Box::new(RustExpr::Ident(format!("({})", args[0]))),
-                    method: "as_ref".to_string(),
-                    args: vec![],
+    Some(RustExpr::Block {
+        stmts: vec![RustStmt::Let {
+            mutable: false,
+            name: "__json_input".to_string(),
+            ty: None,
+            value: RustExpr::Ident(args[0].clone()),
+        }],
+        expr: Some(Box::new(RustExpr::MethodCall {
+            receiver: Box::new(RustExpr::MethodCall {
+                receiver: Box::new(RustExpr::FnCall {
+                    func: Box::new(RustExpr::Path(vec![
+                        "serde_json".to_string(),
+                        "from_str::<serde_json::Value>".to_string(),
+                    ])),
+                    args: vec![RustExpr::MethodCall {
+                        receiver: Box::new(RustExpr::Ident("__json_input".to_string())),
+                        method: "as_ref".to_string(),
+                        args: vec![],
+                    }],
+                }),
+                method: "map".to_string(),
+                args: vec![RustExpr::Closure {
+                    params: vec![RustParam::Named {
+                        name: "v".to_string(),
+                        ty: RustType::Named("_".to_string()),
+                    }],
+                    body: Box::new(RustExpr::MethodCall {
+                        receiver: Box::new(RustExpr::Ident("v".to_string())),
+                        method: "to_string".to_string(),
+                        args: vec![],
+                    }),
+                    is_move: false,
                 }],
             }),
-            method: "map".to_string(),
+            method: "map_err".to_string(),
             args: vec![RustExpr::Closure {
                 params: vec![RustParam::Named {
-                    name: "v".to_string(),
+                    name: "e".to_string(),
                     ty: RustType::Named("_".to_string()),
                 }],
-                body: Box::new(RustExpr::MethodCall {
-                    receiver: Box::new(RustExpr::Ident("v".to_string())),
-                    method: "to_string".to_string(),
-                    args: vec![],
+                body: Box::new(RustExpr::StructInit {
+                    name: "JSONDecodeError".to_string(),
+                    fields: vec![
+                        (
+                            "message".to_string(),
+                            RustExpr::MethodCall {
+                                receiver: Box::new(RustExpr::Ident("e".to_string())),
+                                method: "to_string".to_string(),
+                                args: vec![],
+                            },
+                        ),
+                        (
+                            "line".to_string(),
+                            RustExpr::Cast {
+                                expr: Box::new(RustExpr::MethodCall {
+                                    receiver: Box::new(RustExpr::Ident("e".to_string())),
+                                    method: "line".to_string(),
+                                    args: vec![],
+                                }),
+                                ty: RustType::I64,
+                            },
+                        ),
+                        (
+                            "column".to_string(),
+                            RustExpr::Cast {
+                                expr: Box::new(RustExpr::MethodCall {
+                                    receiver: Box::new(RustExpr::Ident("e".to_string())),
+                                    method: "column".to_string(),
+                                    args: vec![],
+                                }),
+                                ty: RustType::I64,
+                            },
+                        ),
+                    ],
                 }),
                 is_move: false,
             }],
-        }),
-        method: "map_err".to_string(),
-        args: vec![RustExpr::Closure {
-            params: vec![RustParam::Named {
-                name: "e".to_string(),
-                ty: RustType::Named("_".to_string()),
-            }],
-            body: Box::new(RustExpr::StructInit {
-                name: "JSONDecodeError".to_string(),
-                fields: vec![
-                    (
-                        "message".to_string(),
-                        RustExpr::MethodCall {
-                            receiver: Box::new(RustExpr::Ident("e".to_string())),
-                            method: "to_string".to_string(),
-                            args: vec![],
-                        },
-                    ),
-                    (
-                        "line".to_string(),
-                        RustExpr::Cast {
-                            expr: Box::new(RustExpr::MethodCall {
-                                receiver: Box::new(RustExpr::Ident("e".to_string())),
-                                method: "line".to_string(),
-                                args: vec![],
-                            }),
-                            ty: RustType::I64,
-                        },
-                    ),
-                    (
-                        "column".to_string(),
-                        RustExpr::Cast {
-                            expr: Box::new(RustExpr::MethodCall {
-                                receiver: Box::new(RustExpr::Ident("e".to_string())),
-                                method: "column".to_string(),
-                                args: vec![],
-                            }),
-                            ty: RustType::I64,
-                        },
-                    ),
-                ],
-            }),
-            is_move: false,
-        }],
+        })),
     })
 }
 
